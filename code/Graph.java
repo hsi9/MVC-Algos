e6b4f66f5f6cf8d3e7dc544874c820ee8f405120
Prasenjeet <pbiswal93@gatech.edu>
2017-11-07 16:09:18 -0500

    Added the Graph class and RunAlgo Class

---
+++ code/Graph.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Graph
+{
+	HashMap<Integer, HashMap<Integer, Integer>> graph;
+	int numNodes = 0;
+	Graph(int n)
+	{
+		numNodes=n;
+		graph = new HashMap<Integer, HashMap<Integer, Integer>>();
+		for(int i=0;i<n;i++)
+			graph.put(i, new HashMap<Integer, Integer>());
+	}
+
+	public void addEdge(int node1, int node2, int weight) 
+	{
+		//graph[node1][node2]=weight;
+		HashMap<Integer, Integer> first = graph.get(node1);
+		//System.out.println("Printing first: "+first);
+		first.put(node2, weight);
+
+		//graph[node2][node1]=weight;
+		HashMap<Integer, Integer> second = graph.get(node2);
+		//System.out.println("Printing Second :"+second);
+		second.put(node1, weight);
+	}
+
+	public int getNumNodes()
+	{
+		if(graph!=null)
+			return numNodes;
+		else
+			return -1;
+	}	
+
+
+	public void printGraph()
+	{
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i=0;i<numNodes;i++)
+		{
+			map = graph.get(i);
+			System.out.println(i+" : "+map);
+		}
+	}
+
+	public int getEdgeWeight(int node1, int node2)
+	{
+		//return graph[node1][node2];
+		HashMap<Integer, Integer> node1Neighbours = graph.get(node1);
+		if(node1Neighbours==null)
+			return -1;
+		Integer weight = node1Neighbours.get(node2);
+		if(weight==null)
+			return -1;
+		else
+			return weight;
+	}
+}
---
+++ code/RunAlgo.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+
+public class RunAlgo
+{
+	
+	public static void main(String[] args)
+	{
+		Graph graph = null;
+		BufferedReader br=null;
+		BufferedWriter bw = null;
+		String line=null;
+		String[] tokens=null;
+		
+		try
+		{
+			br=new BufferedReader(new FileReader(args[0]));
+			line=br.readLine();
+			tokens=line.split(" ");
+			graph=new Graph(Integer.parseInt(tokens[0]));
+			int i=0;
+			
+			while((line=br.readLine())!=null)
+			{	
+				
+				
+				if(line.equals(""))
+					continue;
+				
+				tokens=line.split(" ");
+				for(String token:tokens)
+				{
+					graph.addEdge(i, Integer.parseInt(token)-1,1);
+				}
+				
+				
+				i++;
+				//graph.printGraph();
+				//System.out.println(line);
+				
+			}
+			graph.printGraph();
+			br.close();
+			//bw.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Check the addEdge method or the printGraph method.")	;
+		}
+	}
+}
